7e3ce369e585f748cd9f952dfc813321fc1c017e
Marko <markostankovic00>
2024-07-05 19:45:47 +0200

    Projekat 3

---
+++ sr3/Web Socket Server/socket.js
@@ -0,0 +1,30 @@
+const WebSocket = require('ws');
+
+const PORT = 8765;
+
+const wsServer = new WebSocket.Server({
+    port: PORT
+});
+
+wsServer.on('connection', function (socket) {
+    // Some feedback on the console
+    console.log("A client just connected");
+
+    // Attach some behavior to the incoming socket
+    socket.on('message', function (msg) {
+        console.log("Received message from client: "  + msg);
+        // socket.send("Take this back: " + msg);
+
+        // Broadcast that message to all connected clients
+        wsServer.clients.forEach(function (client) {
+            client.send(msg);
+        });
+
+    });
+
+    socket.on('close', function () {
+        console.log('Client disconnected');
+    })
+
+});
+
